ffb8c2b8569afcb5c36459fb16f92d0bc0a90284
Alex Sanders <a@snd.rs>
2020-09-25 16:25:35 +0100

    chore: lint fix

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
 	preset: 'ts-jest',
-	testEnvironment: "jest-environment-jsdom-sixteen",
+	testEnvironment: 'jest-environment-jsdom-sixteen',
 	clearMocks: true,
 	collectCoverage: true,
 	collectCoverageFrom: ['src/**/*'],
