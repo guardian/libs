ce57e5cc4e205307660c066330259c68a205f0d4
Alex Sanders <a@snd.rs>
2020-09-25 16:03:01 +0100

    chore: remove babel

--- .babelrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-typescript'],
-    env: {
-        test: {
-            plugins: ['@babel/plugin-transform-runtime'],
-        },
-    },
-};
