4c6a0938c175e868cc765586ef517c5619e2628a
Alex Sanders <a@snd.rs>
2020-09-25 16:02:40 +0100

    chore: update/move configs

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,6 @@
+## What does this change?
+
+
+
+## Why?
+
---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: "jest-environment-jsdom-sixteen",
+	clearMocks: true,
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*'],
+};
