8f598df4cb6698af96eab62646fe5f5b1b1233c9
Alex Sanders <a@snd.rs>
2020-10-21 12:29:49 +0100

    docs: add jsdoc

--- src/loadScript.ts
+++ src/loadScript.ts
@@ -1,6 +1,17 @@
+/**
+ * Loads an external JavaScript file by injecting a `script` element into the page.
+ *
+ * Returns a promise that resolves once the script has loaded, or rejects if something goes wrong.
+ *
+ * If a script has been loaded already, it will resolve immediately.
+ *
+ * @param src - URL for the script `src`
+ * @param props - any valid `script` attributes other than `src`, `onload` or `onerror`
+ */
+
 export const loadScript = (
 	src: string,
-	props?: Omit<Partial<HTMLScriptElement>, 'src' | 'onload' | ' onerror'>,
+	props?: Omit<Partial<HTMLScriptElement>, 'src' | 'onload' | 'onerror'>,
 ): Promise<Event | undefined> =>
 	new Promise((resolve, reject) => {
 		// creating this before the check below allows us to compare the resolved `src` values
