5d6c21ba0f98f8d7e2a163566594dbf97e1b98da
Alex Sanders <a@snd.rs>
2020-10-26 00:04:58 +0000

    simplify lazyProp and make sure it’s not being called multiple times

--- src/lazyProp.test.ts
+++ src/lazyProp.test.ts
@@ -1,21 +1,37 @@
 import { lazyProp } from './lazyProp';
 
-const a: Record<string, unknown> = {};
+const setter = jest.fn(() => 'b');
 
 describe('lazyProp', () => {
 	it('allows you to use a property as normal', () => {
-		expect(a.b).toBeUndefined();
+		const a: Record<string, unknown> = {};
 
-		lazyProp(a, 'b', () => 'b');
+		expect(a.b).toBeUndefined();
+		expect(setter).toHaveBeenCalledTimes(0);
+
+		lazyProp(a, 'b', setter);
+		expect(setter).toHaveBeenCalledTimes(0);
+
 		expect(a.b).toBe('b');
+		expect(setter).toHaveBeenCalledTimes(1);
+
+		expect(a.b).toBe('b');
+		expect(a.b).toBe('b');
+		expect(setter).toHaveBeenCalledTimes(1);
 
 		a.b = 'B,B,B,B,B,B,B,B,B,B,B';
 		expect(a.b).toBe('B,B,B,B,B,B,B,B,B,B,B');
+		expect(setter).toHaveBeenCalledTimes(1);
 
-		lazyProp(a, 'b', () => 'b');
+		lazyProp(a, 'b', setter);
+		expect(setter).toHaveBeenCalledTimes(1);
+
 		expect(a.b).toBe('b');
+		expect(a.b).toBe('b');
+		expect(setter).toHaveBeenCalledTimes(2);
 
 		delete a.b;
 		expect(a.b).toBeUndefined();
+		expect(setter).toHaveBeenCalledTimes(2);
 	});
 });
--- src/lazyProp.ts
+++ src/lazyProp.ts
@@ -1,38 +1,27 @@
 /**
  * Lazily initialises a property on an object when the property is first accessed.
- * https://davidwalsh.name/lazy-object-initialization
+ * Based on https://davidwalsh.name/lazy-object-initialization
  */
 
 export type LazyProp = (
-	hostObj: Record<string, unknown>,
+	object: Record<string, unknown>,
 	name: string,
 	initialiser: () => unknown,
 ) => void;
 
-export const lazyProp: LazyProp = (hostObj, name, initialiser) => {
-	let defined = false;
-
-	Object.defineProperty(hostObj, name, {
-		get: function () {
-			// If property has not been defined, we're going to do it now
-			if (!defined) {
-				// Define the property by assigning the return value of
-				// the initialiser to the property
-				Object.defineProperty(hostObj, name, {
-					configurable: true,
-					enumerable: true,
-					value: initialiser.apply(hostObj),
-					writable: true,
-				});
-
-				// don't do all this more than once
-				defined = true;
-
-				// return the value, since that's what we wanted in the first place
-				return hostObj[name];
-			}
-		},
+export const lazyProp: LazyProp = (object, name, initialiser) => {
+	Object.defineProperty(object, name, {
 		configurable: true,
 		enumerable: true,
+		get: function () {
+			const value = initialiser.apply(this);
+			Object.defineProperty(this, name, {
+				configurable: true,
+				enumerable: true,
+				writable: true,
+				value,
+			});
+			return value;
+		},
 	});
 };
