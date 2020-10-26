8f0654e6855c76426d35d16edf4e3c0a9ccd1a61
Alex Sanders <a@snd.rs>
2020-10-26 00:39:37 +0000

    word change

--- src/lazyProp.ts
+++ src/lazyProp.ts
@@ -6,7 +6,7 @@
  * const x = { a: 'a' };
  * lazyProp(x, 'b', anExpensiveCalculation)
  * // x.b is undefined until it's accessed, like a cat in a box
- * alert(x.b) // anExpensiveCalculation runs and x.b has the value it returns
+ * alert(x.b) // anExpensiveCalculation runs and x.b assumes the value it returns
  * ```
  */
 
