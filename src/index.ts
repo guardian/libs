d7c7aed8334fb9f02f58ce5e68ac0ada3c0b3ab6
Alex Sanders <a@snd.rs>
2020-11-06 16:46:27 +0000

    feat: add isBoolean

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /* istanbul ignore file */
 
 export { getLocale } from './getLocale';
+export { isBoolean } from './isBoolean';
 export { isObject } from './isObject';
 export { isString } from './isString';
 export { loadScript } from './loadScript';
