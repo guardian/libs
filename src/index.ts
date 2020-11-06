a42fb2d3e0214f52b0c723e921a2be88098b78dc
Alex Sanders <a@snd.rs>
2020-11-06 16:46:13 +0000

    feat: add isObject

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /* istanbul ignore file */
 
 export { getLocale } from './getLocale';
+export { isObject } from './isObject';
 export { isString } from './isString';
 export { loadScript } from './loadScript';
 export { storage } from './storage';
---
+++ src/isObject.ts
@@ -0,0 +1,1 @@
+export { default as isObject } from 'is-plain-obj';
