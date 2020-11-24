2b41817abad228fc87d8e19597b58d0bb233688a
Alex Sanders <a@snd.rs>
2020-11-24 12:22:41 +0000

    fix: getLocale return type

--- src/getLocale.ts
+++ src/getLocale.ts
@@ -25,25 +25,23 @@ export const __resetCachedValue = (): void => (locale = void 0);
 export const getLocale = async (): Promise<CountryCode | null> => {
 	if (locale) return locale;
 
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- it _is_ any
-	const stored = storage.local.get(KEY);
+	const stored = storage.local.get(KEY) as CountryCode;
 
 	// if we've got a locale, return it
-	if (isValidCountryCode(stored)) return (locale = stored as CountryCode);
+	if (isValidCountryCode(stored)) return (locale = stored);
 
 	// use our API to get one
 	try {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- it's json
-		const { country } = await fetch(URL).then((response) =>
+		const { country } = (await fetch(URL).then((response) =>
 			response.json(),
-		);
+		)) as { country: CountryCode };
 
 		if (isValidCountryCode(country)) {
 			// save it for 10 days
 			storage.local.set(KEY, country, daysFromNow(10));
 
 			// return it
-			return (locale = country as CountryCode);
+			return (locale = country);
 		}
 	} catch (e) {
 		// do nothing
