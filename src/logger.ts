4f6c323f4a0d24df560ccfdd582a31d456914fb1
Alex Sanders <a@snd.rs>
2020-12-08 18:32:49 +0000

    chore: add team names as types (#79)
    * add team names as types
    
    * disallow using `common`
    
    * disallow using `common`
    
    * simpler exclude
    
    * styles !== team names
    
    * Update src/logger.ts
    
    Co-authored-by: Max Duval <max.duval@guardian.co.uk>
    
    * Update src/logger.ts
    
    Co-authored-by: Max Duval <max.duval@guardian.co.uk>
    
    * no redundant type
    
    * enough contrast for CMP label
    
    Co-authored-by: Max Duval <max@mxdvl.com>
    Co-authored-by: Max Duval <max.duval@guardian.co.uk>

--- src/logger.ts
+++ src/logger.ts
@@ -12,11 +12,7 @@ import { storage } from './storage';
 
 const KEY = 'gu.logger';
 
-type TeamColours = Record<string, Record<string, string>>;
-type LogCall = (team: string, ...args: unknown[]) => void;
-export type TeamFunction = (arg: string) => void;
-
-const teamColours: TeamColours = {
+const teams = {
 	common: {
 		background: '#052962',
 		font: '#ffffff',
@@ -29,9 +25,21 @@ const teamColours: TeamColours = {
 		background: '#000000',
 		font: '#ff7300',
 	},
+	cmp: {
+		background: '#FF3399',
+		font: '#332020',
+	},
 };
 
-const style = (team: string): string => {
+export type Styles = keyof typeof teams;
+export type TeamName = Exclude<Styles, 'common'>;
+type Teams<K extends string> = Record<K, Record<string, string>>;
+const teamColours: Teams<Styles> = teams;
+
+type LogCall = (team: TeamName, ...args: unknown[]) => void;
+export type TeamFunction = (arg: TeamName) => void;
+
+const style = (team: Styles): string => {
 	const { background = 'black' } = { ...teamColours[team] };
 	const { font = 'white' } = { ...teamColours[team] };
 	return `background: ${background}; color: ${font}; padding: 2px; border-radius:3px`;
