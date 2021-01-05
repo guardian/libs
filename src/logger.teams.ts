9a27b1daeb8392ce968a2a69ea705855b8b1d4c4
Tom Wey <tom.wey@theguardian.com>
2021-01-05 16:55:37 +0000

    Add colour descriptions as comments

--- src/logger.teams.ts
+++ src/logger.teams.ts
@@ -6,23 +6,23 @@
  * */
 export const teams = {
 	common: {
-		background: '#052962',
-		font: '#ffffff',
+		background: '#052962', // dark blue
+		font: '#ffffff', // white
 	},
 	commercial: {
-		background: '#77EEAA',
-		font: '#004400',
+		background: '#77EEAA', // mint green
+		font: '#004400', // dark green
 	},
 	cmp: {
-		background: '#FF3399',
-		font: '#332020',
+		background: '#FF3399', // bright pink
+		font: '#332020', // dark brown
 	},
 	dotcom: {
-		background: '#000000',
-		font: '#ff7300',
+		background: '#000000', // black
+		font: '#ff7300', // orange
 	},
 	design: {
-		background: '#185E36',
-		font: '#FFF4F2',
+		background: '#185E36', // green
+		font: '#FFF4F2', // light pink
 	},
 };
