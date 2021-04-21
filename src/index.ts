0ea307f6be55e5e3885b0ddf7035b5f23ef5a7fa
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2021-04-21 16:26:47 +0100

    Alphabetically ordered exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /* istanbul ignore file */
 
 export { getLocale } from './getLocale';
+export { isObject } from './isObject';
 export { isString } from './isString';
 export { isUndefined } from './isUndefined';
 export { loadScript } from './loadScript';
 export { storage } from './storage';
 export { timeAgo } from './timeAgo';
 export { log, debug } from './logger';
-export { isObject } from './isObject';
