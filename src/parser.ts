248ffc28128282cea200259d33f0c2588f335344
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2021-04-30 18:39:09 +0100

    Added docs for most of the parser functions

--- src/parser.ts
+++ src/parser.ts
@@ -22,29 +22,78 @@ const parser = <A>(f: (a: unknown) => Result<string, A>): Parser<A> => ({
 	run: f,
 });
 
+/**
+ * Ignores whatever the input is and instead provides a successful parsing
+ * result containing `a`.
+ * 
+ * @param a The value to be contained in the successful parsing result
+ * @returns {Parser<A>} A `Parser` of `A`
+ * @example
+ * const parseFoo = succeed('foo'); // Parser<string>
+ * 
+ * const result = run(parseFoo)(42); // Ok<string>, with value 'foo'
+ */
 const succeed = <A>(a: A): Parser<A> => parser((_) => ok(a));
 
+/**
+ * Ignores whatever the input is and instead provides a failed parsing
+ * result containing the error string `e`.
+ * 
+ * @param e The error string to be provided as the parsing error
+ * @returns {Parser<A>} A `Parser` of `A`
+ * @example
+ * const parseFoo = fail('Uh oh!'); // Parser<A>
+ * 
+ * const result = run(parseFoo)(42); // Err<string>, with value 'Uh oh!'
+ */
 const fail = <A>(e: string): Parser<A> => parser((_) => err(e));
 
 const isObject = (a: unknown): a is Record<string, unknown> =>
 	typeof a === 'object' && a !== null;
 
+/**
+ * Run a parser over a given input that's currently `unknown`, and attempt
+ * to parse that input into a value of type `A`. If the parsing was successful
+ * the result will be an `Ok<A>`. If it failed the result will be an
+ * `Err<string>`.
+ * 
+ * @param pa The parser to run over the input
+ * @param a The `unknown` input to be parsed into type-safe value of type `A`
+ * @returns {Result<string, A>} A parsing result, wrapped in a `Result` type.
+ * @example
+ * const json: unknown = JSON.parse('{ "foo": 42 }');
+ * 
+ * const parserA = parseField('foo', parseNumber); // Parser<number>
+ * const resultA = run(parserA)(json); // Ok<number>, with value 42
+ * 
+ * const parserB = parseField('bar', parseNumber); // Parser<number>
+ * const resultB = run(parserB)(json); // Err<string>, with 'missing field' err
+ */
 const run = <A>(pa: Parser<A>) => (a: unknown): Result<string, A> => pa.run(a);
 
 // ----- Basic Parsers ----- //
 
+/**
+ * Parses a value into a `string`.
+ */
 const parseString: Parser<string> = parser((a) =>
 	typeof a === 'string'
 		? ok(a)
 		: err(`Unable to parse ${String(a)} as a string`),
 );
 
+/**
+ * Parses a value into a `number`.
+ */
 const parseNumber: Parser<number> = parser((a) =>
 	typeof a === 'number' && !isNaN(a)
 		? ok(a)
 		: err(`Unable to parse ${String(a)} as a number`),
 );
 
+/**
+ * Parses a value into a `Date`.
+ */
 const parseDate: Parser<Date> = parser((a) => {
 	if (typeof a === 'string' || typeof a === 'number' || a instanceof Date) {
 		const d = new Date(a);
@@ -59,11 +108,43 @@ const parseDate: Parser<Date> = parser((a) => {
 	return err(`Can't transform ${String(a)} into a date`);
 });
 
+/**
+ * Makes the value handled by the given parser optional. **Note:** This
+ * will effectively absorb any failure to parse the value, converting it to
+ * a `None` instead.
+ * 
+ * @param pa A parser
+ * @returns {Parser<Option<A>>} A new `Parser` with an optional value
+ * @example
+ * const json: unknown = JSON.parse('{ "foo": 42 }');
+ * 
+ * const parserA = maybe(parseField('bar', parseNumber)); // Parser<Option<number>>
+ * const resultA = run(parserA)(json); // Ok<None>
+ * 
+ * const parserB = parseField('bar', maybe(parseNumber)); // Parser<Option<number>>
+ * const resultB = run(parserB)(json); // Err<string>, with 'missing field' err
+ */
 const maybe = <A>(pa: Parser<A>): Parser<Option<A>> =>
 	parser((a) => ok(toOption(pa.run(a))));
 
 // ----- Data Structure Parsers ----- //
 
+/**
+ * Runs the parser `pa` over the value found at a given field in an object.
+ * Will fail if the input isn't an object or the field is missing.
+ * 
+ * @param field The field containing the value to parse
+ * @param pa The parser to use on the field value
+ * @returns {Parser<A>} A parser for the value at the given field
+ * @example
+ * const json: unknown = JSON.parse('{ "foo": 42 }');
+ * 
+ * const parserA = parseField('foo', parseNumber); // Parser<number>
+ * const resultA = run(parserA)(json); // Ok<number>, with value 42
+ * 
+ * const parserB = parseField('bar', parseNumber); // Parser<number>
+ * const resultB = run(parserB)(json); // Err<string>, with 'missing field' err
+ */
 const parseField = <A>(field: string, pa: Parser<A>): Parser<A> =>
 	parser((a) => {
 		if (!isObject(a)) {
@@ -75,6 +156,22 @@ const parseField = <A>(field: string, pa: Parser<A>): Parser<A> =>
 		return pa.run(a[field]);
 	});
 
+/**
+ * Runs the parser `pa` over the value found at a given index in an array.
+ * Will fail if the input isn't an array or the index is empty.
+ * 
+ * @param index The array index containing the value to parse
+ * @param pa The parser to use on the value at the given index
+ * @returns {Parser<A>} A parser for the value at the given index
+ * @example
+ * const json: unknown = JSON.parse('[41, 42, 43]');
+ * 
+ * const parserA = parseIndex(1, parseNumber); // Parser<number>
+ * const resultA = run(parserA)(json); // Ok<number>, with value 42
+ * 
+ * const parserB = parseIndex(7, parseNumber); // Parser<number>
+ * const resultB = run(parserB)(json); // Err<string>, with 'missing index' err
+ */
 const parseIndex = <A>(index: number, pa: Parser<A>): Parser<A> =>
 	parser((a) => {
 		if (!Array.isArray(a)) {
@@ -92,6 +189,19 @@ const parseIndex = <A>(index: number, pa: Parser<A>): Parser<A> =>
 		return pa.run(value);
 	});
 
+/**
+ * A convenience for parsing values in nested objects. Takes a list of fields
+ * used to pinpoint a location within the nested objects.
+ * 
+ * @param location Field names designating a location inside a nested object
+ * @param pa The parser to use on the value at the location
+ * @returns {Parser<A>} A parser for the value at the given location
+ * @example
+ * const json: unknown = JSON.parse('{ "foo": { "bar": 42 } }');
+ * 
+ * const parser = parseAt(['foo', 'bar'], parseNumber); // Parser<number>
+ * const result = run(parser)(json); // Ok<number>, with value 42
+ */
 const parseAt = <A>(location: string[], pa: Parser<A>): Parser<A> =>
 	parser((a) => {
 		if (location.length === 1) {
@@ -111,6 +221,18 @@ const parseAt = <A>(location: string[], pa: Parser<A>): Parser<A> =>
 		);
 	});
 
+/**
+ * Parses an array containing values of type `A`.
+ * Will fail if the input isn't an array, or the values can't be parsed as `A`.
+ * 
+ * @param pa A parser for the values in the array
+ * @returns {Parser<A[]>} A parser for the array
+ * @example
+ * const json: unknown = JSON.parse('[41, 42, 43]');
+ * 
+ * const parser = parseArray(parseNumber); // Parser<number[]>
+ * const result = run(parser)(json); // Ok<number[]>, with value [41, 42, 43]
+ */
 const parseArray = <A>(pa: Parser<A>): Parser<A[]> =>
 	parser((a) => {
 		const f = (acc: A[], remainder: unknown[]): Result<string, A[]> => {
@@ -137,9 +259,52 @@ const parseArray = <A>(pa: Parser<A>): Parser<A[]> =>
 
 // ----- Combinator Functions ----- //
 
+/**
+ * Converts a `Parser` of `A` to a `Parser` of `B`. Will apply the given
+ * function `f` to the result of the given parser (`Parser<A>`) if that parser
+ * is successful.
+ * 
+ * @param f The function to apply
+ * @param pa The parser to convert
+ * @returns {Parser<B>} A new parser for type `B`
+ * @example
+ * const parser = map(n => n + 1)(parseNumber); // Parser<number>
+ * 
+ * const valueA: unknown = 41;
+ * const resultA = run(parser)(valueA); // Ok<number>, with value 42
+ * 
+ * const valueB: unknown = 'foo';
+ * const resultB = run(parser)(valueB); // Err<string>, with 'not a number' err
+ */
 const map = <A, B>(f: (a: A) => B) => (pa: Parser<A>): Parser<B> =>
 	parser((a) => resultMap(f)(pa.run(a)));
 
+/**
+ * Similar to `map`. Will apply the given function `f` to the results of two
+ * given parsers (`Parser<A>` and `Parser<B>`) if both of those parsers are
+ * successful.
+ * 
+ * @param f A function with two arguments, `a` and `b`, which correspond to the
+ * values potentially returned by the two parsers
+ * @param pa The first parser, for a value of type `A`
+ * @param pb The second parser, for a value of type `B`
+ * @returns {Parser<C>} A new parser for type `C`
+ * @example
+ * type Person = { name: string, age: number };
+ * 
+ * const makePerson = (name: string, age: number): Person => ({ name, age });
+ * const parsePerson: Parser<Person> =
+ *   map2(makePerson)(
+ *     parseField('name', parseString),
+ *     parseField('age', parseNumber),
+ *   );
+ * 
+ * const jsonA: unknown = JSON.parse('{ "name": "CP Scott", "age": 85 }');
+ * const resultA = run(parsePerson)(jsonA); // Ok<Person>
+ * 
+ * const jsonB: unknown = JSON.parse('{ "name": "CP Scott" }');
+ * const resultB = run(parsePerson)(jsonB); // Err<string>, 'missing field' err
+ */
 const map2 = <A, B, C>(f: (a: A, b: B) => C) => (
 	pa: Parser<A>,
 	pb: Parser<B>,
@@ -160,6 +325,10 @@ const map2 = <A, B, C>(f: (a: A, b: B) => C) => (
 		return ok(f(resultA.value, resultB.value));
 	});
 
+/**
+ * Similar to `map2`, but for more parsers. See the docs for that function for
+ * more details and examples.
+ */
 const map3 = <A, B, C, D>(f: (a: A, b: B, c: C) => D) => (
 	pa: Parser<A>,
 	pb: Parser<B>,
@@ -187,6 +356,10 @@ const map3 = <A, B, C, D>(f: (a: A, b: B, c: C) => D) => (
 		return ok(f(resultA.value, resultB.value, resultC.value));
 	});
 
+/**
+ * Similar to `map2`, but for more parsers. See the docs for that function for
+ * more details and examples.
+ */
 const map4 = <A, B, C, D, E>(f: (a: A, b: B, c: C, d: D) => E) => (
 	pa: Parser<A>,
 	pb: Parser<B>,
@@ -223,6 +396,10 @@ const map4 = <A, B, C, D, E>(f: (a: A, b: B, c: C, d: D) => E) => (
 		);
 	});
 
+/**
+ * Similar to `map2`, but for more parsers. See the docs for that function for
+ * more details and examples.
+ */
 const map5 = <A, B, C, D, E, F>(f: (a: A, b: B, c: C, d: D, e: E) => F) => (
 	pa: Parser<A>,
 	pb: Parser<B>,
@@ -272,6 +449,10 @@ const map5 = <A, B, C, D, E, F>(f: (a: A, b: B, c: C, d: D, e: E) => F) => (
 		);
 	});
 
+/**
+ * Similar to `map2`, but for more parsers. See the docs for that function for
+ * more details and examples.
+ */
 const map6 = <A, B, C, D, E, F, G>(
 	f: (a: A, b: B, c: C, d: D, e: E, f: F) => G,
 ) => (
@@ -331,6 +512,10 @@ const map6 = <A, B, C, D, E, F, G>(
 		);
 	});
 
+/**
+ * Similar to `map2`, but for more parsers. See the docs for that function for
+ * more details and examples.
+ */
 const map7 = <A, B, C, D, E, F, G, H>(
 	f: (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => H,
 ) => (
@@ -398,6 +583,10 @@ const map7 = <A, B, C, D, E, F, G, H>(
 		);
 	});
 
+/**
+ * Similar to `map2`, but for more parsers. See the docs for that function for
+ * more details and examples.
+ */
 const map8 = <A, B, C, D, E, F, G, H, I>(
 	f: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => I,
 ) => (
@@ -473,6 +662,38 @@ const map8 = <A, B, C, D, E, F, G, H, I>(
 		);
 	});
 
+/**
+ * Like `map` but applies a function that *also* returns an `Parser`. Then
+ * "unwraps" the result for you so you don't end up with `Parser<Parser<A>>`.
+ * Can be useful for stringing together multiple parsing steps, where each step
+ * relies on the result of the previous one.
+ * 
+ * If the first parser fails, the function won't be called and the error will
+ * be returned instead. If the second parser fails the error for that will be
+ * returned.
+ * 
+ * @param f The function to apply
+ * @param pa The parser whose result is to be passed to the function
+ * @return {Parser<B>} A new parser, built from `pa` and `f`
+ * @example
+ * type MultipleChoiceAnswer = 'a' | 'b' | 'c';
+ * 
+ * const parseAnswerValue = (value: string): Parser<MultipleChoiceAnswer> =>
+ *   value === 'a' || value === 'b' || value === 'c'
+ *     ? succeed(value)
+ *     : fail("Needed 'a', 'b' or 'c'.");
+ * 
+ * const parseAnswer = pipe(
+ *   parseField('answer', parseString),
+ *   andThen(parseAnswerValue),
+ * );
+ * 
+ * const jsonA: unknown = JSON.parse('{ "answer": "a" }');
+ * const resultA = run(parseAnswer)(jsonA); // Ok<MultipleChoiceAnswer>, 'a'
+ * 
+ * const jsonB: unknown = JSON.parse('{ "answer": "d" }');
+ * const resultB = run(parseAnswer)(jsonB); // Err<string>, "Needed 'a', 'b' or 'c'."
+ */
 const andThen = <A, B>(f: (a: A) => Parser<B>) => (pa: Parser<A>): Parser<B> =>
 	parser((a) => resultAndThen<string, A, B>((x) => f(x).run(a))(pa.run(a)));
 
