6867664709b2efda52f2557d038eefbb85a3c9c7
Alex Sanders <alex@sndrs.dev>
2021-05-11 12:16:47 +0100

    restore tests

--- src/getLocale.test.ts
+++ src/getLocale.test.ts
@@ -1,63 +1,62 @@
 import fetchMock from 'jest-fetch-mock';
-// import * as cookies from './cookies';
-// import { __resetCachedValue, getLocale } from './getLocale';
-// import { storage } from './storage';
+import * as cookies from './cookies';
+import { __resetCachedValue, getLocale } from './getLocale';
+import { storage } from './storage';
 
-// const KEY = 'GU_geo_country';
-// const KEY_OVERRIDE = 'gu.geo.override';
+const KEY = 'GU_geo_country';
+const KEY_OVERRIDE = 'gu.geo.override';
 
 fetchMock.enableMocks();
 
 describe('getLocale', () => {
-	it("shouldn't fail", () => undefined);
-	// beforeEach(() => {
-	// 	storage.local.clear();
-	// 	cookies.removeCookie({ name: KEY });
-	// 	__resetCachedValue();
-	// });
-	// it('returns overridden locale if it exists', async () => {
-	// 	storage.local.set(KEY_OVERRIDE, 'CY');
-	// 	cookies.setSessionCookie({ name: KEY, value: 'GB' });
-	// 	const locale = await getLocale();
-	// 	expect(locale).toBe('CY');
-	// });
-	// it('gets a stored valid locale', async () => {
-	// 	cookies.setSessionCookie({ name: KEY, value: 'CY' });
-	// 	const locale = await getLocale();
-	// 	expect(locale).toBe('CY');
-	// });
-	// it('fetches the remote value if cookie is missing', async () => {
-	// 	fetchMock.mockResponseOnce(JSON.stringify({ country: 'CZ' }));
-	// 	const locale = await getLocale();
-	// 	expect(locale).toBe('CZ');
-	// 	expect(cookies.getCookie({ name: KEY })).toBe('CZ');
-	// });
-	// it('ignores a stored invalid locale', async () => {
-	// 	fetchMock.mockResponseOnce(JSON.stringify({ country: 'CZ' }));
-	// 	cookies.setSessionCookie({ name: KEY, value: 'outerspace' });
-	// 	const locale = await getLocale();
-	// 	expect(locale).toBe('CZ');
-	// 	expect(cookies.getCookie({ name: KEY })).toBe('CZ');
-	// });
-	// it('ignores an invalid remote response', async () => {
-	// 	fetchMock.mockResponseOnce(JSON.stringify({ country: 'outerspace' }));
-	// 	const locale = await getLocale();
-	// 	expect(locale).toBeNull();
-	// 	expect(cookies.getCookie({ name: KEY })).toBeNull();
-	// });
-	// it('ignores an error in the remote response', async () => {
-	// 	fetchMock.mockResponseOnce('regregergreg');
-	// 	const locale = await getLocale();
-	// 	expect(locale).toBeNull();
-	// 	expect(cookies.getCookie({ name: KEY })).toBeNull();
-	// });
-	// it('uses the cached value if available', async () => {
-	// 	const spy = jest.spyOn(cookies, 'getCookie');
-	// 	cookies.setSessionCookie({ name: KEY, value: 'CY' });
-	// 	const locale = await getLocale();
-	// 	const locale2 = await getLocale();
-	// 	expect(locale).toBe(locale2);
-	// 	expect(spy).toHaveBeenCalledTimes(1);
-	// 	expect(fetchMock).not.toHaveBeenCalled();
-	// });
+	beforeEach(() => {
+		storage.local.clear();
+		cookies.removeCookie({ name: KEY });
+		__resetCachedValue();
+	});
+	it('returns overridden locale if it exists', async () => {
+		storage.local.set(KEY_OVERRIDE, 'CY');
+		cookies.setSessionCookie({ name: KEY, value: 'GB' });
+		const locale = await getLocale();
+		expect(locale).toBe('CY');
+	});
+	it('gets a stored valid locale', async () => {
+		cookies.setSessionCookie({ name: KEY, value: 'CY' });
+		const locale = await getLocale();
+		expect(locale).toBe('CY');
+	});
+	it('fetches the remote value if cookie is missing', async () => {
+		fetchMock.mockResponseOnce(JSON.stringify({ country: 'CZ' }));
+		const locale = await getLocale();
+		expect(locale).toBe('CZ');
+		expect(cookies.getCookie({ name: KEY })).toBe('CZ');
+	});
+	it('ignores a stored invalid locale', async () => {
+		fetchMock.mockResponseOnce(JSON.stringify({ country: 'CZ' }));
+		cookies.setSessionCookie({ name: KEY, value: 'outerspace' });
+		const locale = await getLocale();
+		expect(locale).toBe('CZ');
+		expect(cookies.getCookie({ name: KEY })).toBe('CZ');
+	});
+	it('ignores an invalid remote response', async () => {
+		fetchMock.mockResponseOnce(JSON.stringify({ country: 'outerspace' }));
+		const locale = await getLocale();
+		expect(locale).toBeNull();
+		expect(cookies.getCookie({ name: KEY })).toBeNull();
+	});
+	it('ignores an error in the remote response', async () => {
+		fetchMock.mockResponseOnce('regregergreg');
+		const locale = await getLocale();
+		expect(locale).toBeNull();
+		expect(cookies.getCookie({ name: KEY })).toBeNull();
+	});
+	it('uses the cached value if available', async () => {
+		const spy = jest.spyOn(cookies, 'getCookie');
+		cookies.setSessionCookie({ name: KEY, value: 'CY' });
+		const locale = await getLocale();
+		const locale2 = await getLocale();
+		expect(locale).toBe(locale2);
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(fetchMock).not.toHaveBeenCalled();
+	});
 });
