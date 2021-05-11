8e4555af952782fe2e76c08751306f1408a3b5c8
Alex Sanders <alex@sndrs.dev>
2021-05-11 15:26:08 +0100

    move country types to `@guardian/types`

--- src/getLocale.ts
+++ src/getLocale.ts
@@ -1,7 +1,7 @@
+import type { CountryCode } from '@guardian/types';
 import { getCookie, setSessionCookie } from './cookies';
 import { isString } from './isString';
 import { storage } from './storage';
-import type { CountryCode } from './types/countries';
 
 const KEY = 'GU_geo_country';
 const KEY_OVERRIDE = 'gu.geo.override';
