83fd9764ec68de61924ae548e4c9f260fde8103d
SiAdcock <simonadcock2@gmail.com>
2021-05-26 22:56:06 +0100

    fix lint issues

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,18 @@ export { getLocale } from './getLocale';
 export { isString } from './isString';
 export { isUndefined } from './isUndefined';
 export { loadScript } from './loadScript';
-export { OptionKind,
+export {
+	OptionKind,
 	some,
 	none,
 	fromNullable,
 	withDefault,
 	map,
 	map2,
-	andThen, } from './option';
-export { 	ResultKind,
+	andThen,
+} from './option';
+export {
+	ResultKind,
 	ok,
 	err,
 	fromUnsafe,
@@ -21,7 +24,8 @@ export { 	ResultKind,
 	mapError,
 	toOption,
 	map as resultMap,
-	andThen as resultAndThen, } from './result';
+	andThen as resultAndThen,
+} from './result';
 export { storage } from './storage';
 export { timeAgo } from './timeAgo';
 export {
