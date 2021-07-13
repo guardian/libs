654a80f46e7f004f40524be14f058b32b6efe1e9
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-07-13 13:48:34 +0100

    feat: migrate format from `@guardian/types` (#220)
    * migrate format from types
    
    * chore(docs): update TOC
    
    * Add tests for format enums
    
    * Fix heading order in README
    
    Co-authored-by: Jamie B <53781962+JamieB-gu@users.noreply.github.com>
    
    * add Design.Obituary
    
    * chore(docs): update TOC
    
    * nothing
    
    * add Article prefix to format exports
    
    Co-authored-by: jamie-lynch <jamie-lynch@users.noreply.github.com>
    Co-authored-by: Jamie B <53781962+JamieB-gu@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ export {
 export type { Country } from './countries';
 export { countries, getCountryByCountryCode } from './countries';
 export type { CountryCode } from './@types/countries';
+export type { ArticleTheme, ArticleFormat } from './format';
+export {
+	ArticlePillar,
+	ArticleSpecial,
+	ArticleDesign,
+	ArticleDisplay,
+} from './format';
 export { getLocale } from './getLocale';
 export { getSwitches } from './getSwitches';
 export { isBoolean } from './isBoolean';
