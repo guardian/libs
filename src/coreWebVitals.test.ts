356bcdbc58c84d1a61d45e754bff68004501fbb8
Max Duval <max.duval@guardian.co.uk>
2021-09-13 14:40:03 +0100

    add tests for sendData

--- src/coreWebVitals.test.ts
+++ src/coreWebVitals.test.ts
@@ -1,6 +1,53 @@
-import { _ } from './coreWebVitals';
+import type { Metric, ReportHandler } from 'web-vitals';
+import type { CoreWebVitalsPayload } from './coreWebVitals';
+import { _, coreWebVitals } from './coreWebVitals';
 
 const { roundWithDecimals, sendData } = _;
+
+const coreWebVitalsPayload: CoreWebVitalsPayload = {
+	page_view_id: '123456',
+	browser_id: 'abcdef',
+	fid: 50.5,
+	fcp: 100.1,
+	lcp: 150,
+	ttfb: 9.99,
+	cls: 0.01,
+};
+
+jest.mock('web-vitals', () => ({
+	getTTFB: (onReport: ReportHandler) => {
+		onReport({
+			value: coreWebVitalsPayload.ttfb,
+			name: 'TTFB',
+		} as Metric);
+	},
+	getFCP: (onReport: ReportHandler) => {
+		onReport({
+			value: coreWebVitalsPayload.fcp,
+			name: 'FCP',
+		} as Metric);
+	},
+	getCLS: (onReport: ReportHandler) => {
+		onReport({
+			value: coreWebVitalsPayload.cls,
+			name: 'CLS',
+		} as Metric);
+	},
+	getFID: (onReport: ReportHandler) => {
+		onReport({
+			value: coreWebVitalsPayload.fid,
+			name: 'FID',
+		} as Metric);
+	},
+	getLCP: (onReport: ReportHandler) => {
+		onReport({
+			value: coreWebVitalsPayload.lcp,
+			name: 'LCP',
+		} as Metric);
+	},
+}));
+
+navigator.sendBeacon = jest.fn();
 
 describe('coreWebVitals', () => {
 	it.todo('registers callbacks');
@@ -25,5 +72,37 @@ describe('roundWithDecimals', () => {
 		const [before, after] = [12345.000_001_2, 12345.000_001];
 		expect(roundWithDecimals(before)).toBe(after);
 		expect(roundWithDecimals(before, 6)).toBe(after);
+	});
+});
+
+describe('sendData', () => {
+	beforeAll(() => {
+		jest.spyOn(global.Math, 'random');
+	});
+
+	afterAll(() => {
+		jest.spyOn(global.Math, 'random').mockRestore();
+	});
+
+	beforeEach(() => {
+		jest.resetModules();
+	});
+
+	it('should not send data if not in sample', () => {
+		(global.Math.random as jest.Mock).mockReturnValue(2 / 10);
+		coreWebVitals({
+			browserId: String(coreWebVitalsPayload.browser_id),
+			pageViewId: String(coreWebVitalsPayload.page_view_id),
+		});
+		expect(sendData()).toBe(false);
+	});
+	it('should send data if in sample', () => {
+		(global.Math.random as jest.Mock).mockReturnValue(0.09 / 100);
+		coreWebVitals({
+			browserId: String(coreWebVitalsPayload.browser_id),
+			pageViewId: String(coreWebVitalsPayload.page_view_id),
+		});
+		(navigator.sendBeacon as jest.Mock).mockReturnValue(true);
+		expect(sendData()).toBe(true);
 	});
 });
