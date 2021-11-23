318ea08478e748ff529b571018308b0da1a8aa56
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-11-23 09:38:52 +0000

    feat: Added `whenVisible`

---
+++ src/whenVisible.ts
@@ -0,0 +1,20 @@
+/**
+ * Use this function to delay execution of something until an element is visible
+ * in the viewport
+ *
+ * @param marker : The html element that we want to observe;
+ * @param callback : This is fired when the marker is visible in the viewport
+ */
+export const whenVisible = (
+	marker: HTMLElement,
+	callback: () => void,
+): void => {
+	const io = new IntersectionObserver(([entry]) => {
+		if (!entry.isIntersecting) return;
+		// Disconnect this IntersectionObserver once seen
+		io.disconnect();
+		callback();
+	});
+
+	io.observe(marker);
+};
