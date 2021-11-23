3f20acf7b0bef8f2cd803ed29e7ecb1bf4bc1ebe
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-11-23 10:09:50 +0000

    feat: Allow an options object to passed containing a timeout

--- src/whenIdle.ts
+++ src/whenIdle.ts
@@ -2,10 +2,19 @@
  * whenIdle executes the given callback when the browser is 'idle'
  *
  * @param callback Fired when requestIdleCallback runs or, if requestIdleCallback is not available, after 300ms
+ * @param options Options for requestIdleCallback
+ * @param options.timeout How long to wait for requestIdleCallback to return, defaults to 500ms
  */
-export const whenIdle = (callback: () => void): void => {
+export const whenIdle = (
+	callback: () => void,
+	options: {
+		timeout: number;
+	} = {
+		timeout: 500,
+	},
+): void => {
 	if ('requestIdleCallback' in window) {
-		window.requestIdleCallback(callback, { timeout: 500 });
+		window.requestIdleCallback(callback, options);
 	} else {
 		setTimeout(callback, 300);
 	}
